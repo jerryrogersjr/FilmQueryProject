61e4e36315cfee74417480b2a7eac00c1a6c5aca
Jerry Rogers <jerryrogersjr@gmail.com>
2019-10-25 20:15:47 -0600

    basics

--- src/com/skilldistillery/filmquery/app/FilmQueryApp.java
+++ src/com/skilldistillery/filmquery/app/FilmQueryApp.java
@@ -7,30 +7,31 @@ import com.skilldistillery.filmquery.database.DatabaseAccessorObject;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class FilmQueryApp {
-  
-  DatabaseAccessor db = new DatabaseAccessorObject();
 
-  public static void main(String[] args) {
-    FilmQueryApp app = new FilmQueryApp();
-    app.test();
-//    app.launch();
-  }
+	DatabaseAccessor db = new DatabaseAccessorObject();
 
-  private void test() {
-    Film film = db.findFilmById(1);
-    System.out.println(film);
-  }
+	public static void main(String[] args) {
+		FilmQueryApp app = new FilmQueryApp();
+//		app.test();
+		app.launch();
 
-  private void launch() {
-    Scanner input = new Scanner(System.in);
-    
-    startUserInterface(input);
-    
-    input.close();
-  }
+	}
 
-  private void startUserInterface(Scanner input) {
-    
-  }
+//	private void test() { // this method goes away
+//		Film film = db.findFilmById(1);
+//		System.out.println(film);
+//	}
+
+	private void launch() {
+		Scanner input = new Scanner(System.in);
+
+		startUserInterface(input);
+
+		input.close();
+	}
+
+	private void startUserInterface(Scanner input) {
+		// menu goes here
+	}
 
 }
--- src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
+++ src/com/skilldistillery/filmquery/database/DatabaseAccessorObject.java
@@ -1,16 +1,32 @@
 package com.skilldistillery.filmquery.database;
 
+import java.util.List;
+
+import com.skilldistillery.filmquery.entities.Actor;
 import com.skilldistillery.filmquery.entities.Film;
 
 public class DatabaseAccessorObject implements DatabaseAccessor {
 
-  @Override
-  public Film findFilmById(int filmId) {
-    return null;
-    
-    //TODO 
+	@Override
+	public Film findFilmById(int filmId) {
+		return null;
+
+		// TODO
 //  public Actor findActorById(int actorId);
 //  public List<Actor> findActorsByFilmId(int filmId);
-  }
+
+	}
+
+	@Override
+	public Actor findActorById(int actorId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Actor> findActorsByFilmId(int filmId) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
